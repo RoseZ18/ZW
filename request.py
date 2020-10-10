20ba2090e146aa7345cb3518774175181eeb11e2
RoseZ <2283156822@qq.com>
2020-10-10 20:40:48 +0800

    请求题目

---
+++ request.py
@@ -0,0 +1,15 @@
+import requests
+import base64
+def request():
+    url = "http://47.102.118.1:8089/api/problem" + "?stuid=031802245 }"
+    # 发送get请求
+    r = requests.get(url)
+    #获取返回的json数据
+    user_dic = r.json()
+    data = user_dic['img']
+    step=user_dic['step']
+    uuid=user_dic['uuid']
+    swap = user_dic['swap']
+    image_data = base64.b64decode(data)#解码图片
+    return image_data,step,swap,uuid,
+
